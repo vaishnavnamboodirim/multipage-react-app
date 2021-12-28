66b0e7b4e4f5bc6212cc09f21cd5e28ce2eb8637
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2021-12-29 04:55:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MainPage from "./pages";
+import UsersPage from './pages/users';
 
+function App() {
+  
+    return (
+      <Router>
+        <Routes>
+       {/*All our Routes goes here!*/}
+       <Route path="/" element ={ <MainPage/>} />
+       <Route path="/users" element ={ <UsersPage/>} />
+       
+      </Routes>
+      </Router>
+    );
+  
+}
+
+export default App;
